16cd2b4aeaa1827375013c7deb15579d81c522a4
grian32 <grianguy32@gmail.com>
2025-08-24 17:05:28 +0300

    feat: render tex

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <cstdio>
+#include <iostream>
+#include <ostream>
 
 #include "input.h"
 #include "render/Shader.h"
 #include "util.h"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -42,15 +45,15 @@ int main() {
 
     float vertices[] = {
         // pos              // col
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
-       -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-        0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,// top right
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  1.0f, 1.0f, // bottom right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  1.0f, 0.0f, // bottom left
+        -0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  0.0f, 0.0f,// top right
+        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f,  0.0f, 1.0f,// top right
     };
 
-    float texCoords[] = {
-        0.0f, 0.0f, // lower left
-        1.0f, 0.0f, // lower right
-        0.5f, 1.0f // top center
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     unsigned int VAO;
@@ -62,12 +65,34 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     Shader shaderProgram("../shaders/v.glsl", "../shaders/f.glsl");
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    int width, height, nrChannels;
+    // no clue why png doesnt work, maybe to do with how ase exports ??
+    unsigned char *data = stbi_load("../grass.jpg", &width, &height, &nrChannels, 0);
+
+    unsigned int tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -75,10 +100,12 @@ int main() {
         glClearColorRgb(135, 206, 235);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram.ID);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, tex);
 
+        shaderProgram.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
--- shaders/f.glsl
+++ shaders/f.glsl
@@ -1,9 +1,11 @@
 #version 330 core
-
-in vec3 col;
-
 out vec4 FragColor;
 
+in vec3 col;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+
 void main() {
-    FragColor = vec4(col, 1.0);
+    FragColor = texture(ourTexture, TexCoord);
 }
