3df57c93d72d0a17d60d108515335ee3aec7fbc6
grian32 <grianguy32@gmail.com>
2025-08-24 17:33:26 +0300

    feat: tilt x in v shader

--- shaders/v.glsl
+++ shaders/v.glsl
@@ -6,8 +6,17 @@ layout (location = 2) in vec2 aTexCoord;
 out vec3 col;
 out vec2 TexCoord;
 
+mat3 rotateX(float angle) {
+    float c = cos(angle);
+    float s = sin(angle);
+
+    return mat3(1, 0, 0,
+            0, c, -s,
+            0, s, c);
+}
+
 void main() {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = vec4(rotateX(200) * aPos, 1.0);
     col = aCol;
     TexCoord = aTexCoord;
 }
